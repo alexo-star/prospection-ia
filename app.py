427a016dc59c76f97aff75f0bad7328487978ad0
aymen_ouss <65445139+alexo-star@users.noreply.github.com>
2025-03-26 13:53:54 +0100

    app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def prospect():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
